ce1ccce426ea97544545599cab29e2dc25fc7017
victoriad01 <94958284+victoriad01@users.noreply.github.com>
2023-03-07 18:07:53 +0100

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className='App'>
+      <div className='h-full'>
+        <p className='flex justify-center items-center   text-[#D45769] font-semibold text-[1.2rem]'>
+          Hello, TREM ZHQ AKURE WORKER'S PORTAL
+        </p>
+        <p className='text-[#D4CFC9] text-[0.85rem] my-6'>
+          @ {new Date().getFullYear()}
+        </p>
+      </div>
+    </div>
+  )
+}
+
+export default App
